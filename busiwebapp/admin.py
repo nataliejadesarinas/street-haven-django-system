94486e25d7bbb8df07f49c9289ce9b1d9a6aab02
johnmarkmadrigal-14 <johnmark.madrigal@gsfe.tupcavite.edu.ph>
2026-03-12 14:49:24 +0800

    Models updated, requirement.txt (please download), admin update

--- busiwebapp/admin.py
+++ busiwebapp/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Admin, Apparels, Brand, Category, Shoes, Toys
+admin.site.register(Admin)
+
+@admin.register(Brand)
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+@admin.register(Shoes)
+class ShoesAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'stock', 'category', 'brand', 'is_available')
+    list_filter = ('category', 'brand', 'is_available')
+    search_fields = ('name', 'description')
+
+@admin.register(Apparels)
+class ApparelsAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'stock', 'category', 'brand', 'is_available')
+    list_filter = ('category', 'brand', 'is_available')
+    search_fields = ('name', 'description')
+
+@admin.register(Toys)
+class ToysAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'stock', 'color', 'is_available')
+    list_filter = ('color', 'is_available')
+    search_fields = ('name', 'description')
--- busiwebapp/models.py
+++ busiwebapp/models.py
@@ -1,3 +1,62 @@
 from django.db import models
 
-# Create your models here.
+class Admin(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+    
+class Brand(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+    
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Shoes(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.FloatField()
+    stock = models.IntegerField()
+    description = models.TextField()
+    image = models.ImageField(upload_to='shoes/')
+    color = models.CharField(max_length=50)
+    gender = models.CharField(max_length=10)
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
+    brand = models.ForeignKey('Brand', on_delete=models.CASCADE)
+    is_available = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
+    
+class Apparels(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.FloatField()
+    stock = models.IntegerField()
+    description = models.TextField()
+    image = models.ImageField(upload_to='apparels/')
+    color = models.CharField(max_length=50)
+    gender = models.CharField(max_length=10)
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
+    brand = models.ForeignKey('Brand', on_delete=models.CASCADE)
+    is_available = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
+    
+class Toys(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.FloatField()
+    stock = models.IntegerField()
+    description = models.TextField()
+    image = models.ImageField(upload_to='toys/')
+    color = models.CharField(max_length=50)
+    is_available = models.BooleanField(default=True)
+    def __str__(self):
+        return self.name
